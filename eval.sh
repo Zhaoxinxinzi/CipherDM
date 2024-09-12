c22e0eae7ad784a95a64a644d5455b123424f3b9
Xin Zhao <zx180438@mail.ustc.edu.cn>
2024-09-12 21:36:29 +0800

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -1,2 +1,4 @@
-export PYTHONPATH=./ \
-&& python scripts/flax_sample.py \
+unset http_proxy 
+unset https_proxy 
+nohup python3 nodectl.py -c 3pc.json up >log.txt 2>&1 & 
+export PYTHONPATH=./ && python scripts/flax_sample_hack.py 2>&result.txt &
